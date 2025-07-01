4a4f4d82f0f88f9c5e95af5b87f2613d42e4750a
Nareko <narekomichigami@gmail.com>
2025-07-01 09:24:22 +0300

    base with nothing

---
+++ src/main/java/nareko/fumo/FumoClient.java
@@ -0,0 +1,10 @@
+package nareko.fumo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FumoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
